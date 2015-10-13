f23ef26f71eb7d6c582b48d17c29b80b71700c6a
Will Johansson <will.johansson@gmail.com>
2015-10-12 20:38:16 -0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    slack_name: process.env.SLACK_NAME || 'Awesome Team',
+    slack_url: process.env.SLACK_URL || 'awesome-team.slack.com',
+    slack_token: process.env.SLACK_TOKEN || 'awesome-team-access-token'
+};
