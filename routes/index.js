34942ad0848cc76422202cff421d549f18ecb016
Will Johansson <will.johansson@gmail.com>
2015-10-12 21:03:06 -0700

    Basic introductory text

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sign up for '.concat(config.slack_name), slack_name: config.slack_name });
 });
 
 module.exports = router;
