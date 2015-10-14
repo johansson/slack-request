363cc078d42a612d7f709024089ef7367fd68ebc
Will Johansson <will.johansson@gmail.com>
2015-10-14 10:16:22 -0700

    Correct ENV var naming for conditional error

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     mongo_db: process.env.MONGO_DB || 'localhost/slack-request',
     signup_description: process.env.SIGNUP_DESC || 'You can sign up for Awesome Team by filling out the form:',
     conditional: process.env.CONDITIONAL || 'Do you identify with this group?',
-    conditional_error: process.env.CONDITIONAL || 'You must identify with this group!',
+    conditional_error: process.env.CONDITIONAL_ERROR || 'You must identify with this group!',
     code_of_conduct: process.env.CODE_OF_CONDUCT || 'http://awesome-team.com/codeofconduct',
     admins: { 'foo@bar.com': { password: 'p@$$w0rd!' } }
 };
