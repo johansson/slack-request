f2c8174da8d0d0c255092ad43aade3fe230e353f
James Tranovich <jtranovich@gmail.com>
2015-11-18 20:01:48 -0800

    Default config db value now pulls from docker env.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     slack_name: process.env.SLACK_NAME || 'Awesome Team',
     slack_url: process.env.SLACK_URL || 'awesome-team.slack.com',
     slack_token: process.env.SLACK_TOKEN || 'awesome-team-access-token',
-    mongo_db: process.env.MONGO_DB || 'localhost/slack-request',
+    mongo_db: (process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost)' + '/slack-request',
     signup_description: process.env.SIGNUP_DESC || 'You can sign up for Awesome Team by filling out the form:',
     conditional: process.env.CONDITIONAL || 'Do you identify with this group?',
     conditional_error: process.env.CONDITIONAL_ERROR || 'You must identify with this group!',
