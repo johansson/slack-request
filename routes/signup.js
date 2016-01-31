3f46f03b5a0fa9584cf460049e89368b77884de5
Will Johansson <will.johansson@gmail.com>
2016-01-30 23:50:31 -0800

    Don't erase forms if person messed up. fixes #6

--- routes/signup.js
+++ routes/signup.js
@@ -12,39 +12,61 @@ function to_error_dict(obj) {
     return err_dict;
 }
 
+function checkBody(req) {
+  req.checkBody('firstName', ' (required)').notEmpty();
+  req.checkBody('lastName', ' (required)').notEmpty();
+  req.checkBody('email', ' (invalid e-mail)').notEmpty().isEmail();
+  req.checkBody('handle', ' (required)').notEmpty();
+  req.checkBody('desc', ' (required)').notEmpty();
+  req.checkBody('conditional', config.conditional_error).notEmpty();
+  req.checkBody('codeOfConduct', 'You must agree to the Code of Conduct').notEmpty();
+  req.checkBody('g-recaptcha-response', 'You must pass reCAPTCHA').notEmpty();
+}
+
+function sanitizeBody(req) {
+  req.body.firstName = xssFilters.inHTMLData(req.sanitizeBody('firstName').toString().trim());
+  req.body.lastName = xssFilters.inHTMLData(req.sanitizeBody('lastName').toString().trim());
+  req.body.email = req.sanitizeBody('email').normalizeEmail();
+  req.body.handle = xssFilters.inHTMLData(req.sanitizeBody('handle').toString().trim());
+  req.body.desc = xssFilters.inHTMLData(req.sanitizeBody('desc').toString());
+  req.body.conditional = req.sanitizeBody('conditional').toBoolean();
+  req.body.codeOfConduct = req.sanitizeBody('codeOfConduct').toBoolean();
+}
+
+function assembleInfo(req) {
+  return {
+    "firstName": req.body.firstName,
+    "lastName": req.body.lastName,
+    "email": req.body.email,
+    "handle": req.body.handle,
+    "desc": req.body.desc,
+    "conditional": req.body.conditional,
+    "codeOfConduct": req.body.codeOfConduct,
+    "date": new Date()
+  }
+}
+
 router.get('/', function (req, res) {
-  res.render('signup', { activePage: 'signup', errors: {}, title: 'Sign up for '.concat(config.slack_name), signup_description:config.signup_description, conditional:config.conditional, code_of_conduct:config.code_of_conduct, slack_name: config.slack_name, recaptcha_sitekey: config.recaptcha_sitekey });
+  var viewModel = {
+    activePage: 'signup',
+    errors: {},
+    title: 'Sign up for '.concat(config.slack_name),
+    signup_description: config.signup_description,
+    conditional: config.conditional,
+    code_of_conduct: config.code_of_conduct,
+    slack_name: config.slack_name,
+    recaptcha_sitekey: config.recaptcha_sitekey
+  }
+  res.render('signup', viewModel);
 });
 
 router.post('/', function (req, res) {
-    req.checkBody('firstName', 'First name is required').notEmpty();
-    req.checkBody('lastName', 'Last name is required').notEmpty();
-    req.checkBody('email', 'A valid e-mail is required').notEmpty().isEmail();
-    req.checkBody('handle', 'A handle is required').notEmpty();
-    req.checkBody('description', 'Please describe yourself').notEmpty();
-    req.checkBody('conditional', config.conditional_error).notEmpty();
-    req.checkBody('codeOfConduct', 'You must agree to the Code of Conduct').notEmpty();
-    req.checkBody('g-recaptcha-response', 'You must pass reCAPTCHA').notEmpty();
+    checkBody(req);
+    sanitizeBody(req);
 
-    /* in case we call next() in the future */
-    req.body.firstName = xssFilters.inHTMLData(req.sanitizeBody('firstName').toString().trim());
-    req.body.lastName = xssFilters.inHTMLData(req.sanitizeBody('lastName').toString().trim());
-    req.body.email = req.sanitizeBody('email').normalizeEmail();
-    req.body.handle = xssFilters.inHTMLData(req.sanitizeBody('handle').toString().trim());
-    req.body.description = xssFilters.inHTMLData(req.sanitizeBody('description').toString());
-    req.body.conditional = req.sanitizeBody('conditional').toBoolean();
-    req.body.codeOfConduct = req.sanitizeBody('codeOfConduct').toBoolean();
     var recaptcha = xssFilters.inHTMLData(req.sanitizeBody('g-recaptcha-response').toString());
-
     var errors = req.validationErrors();
-
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    var handle = req.body.handle;
-    var description = req.body.description;
-    var conditional = req.body.conditional;
-    var codeOfConduct = req.body.codeOfConduct;
+    var info = assembleInfo(req);
 
     require('request').post({
         url: 'https://www.google.com/recaptcha/api/siteverify',
@@ -59,17 +81,27 @@ router.post('/', function (req, res) {
     if (!errors) {
         var db = req.db;
         var signups = db.get('signups');
-        signups.insert({ "firstName": firstName, "lastName": lastName, "email": email, "handle": handle, "description": description, "conditional": conditional, "codeOfConduct": codeOfConduct, "date": new Date() }, function (err, doc) {
+        signups.insert(info, function (err, doc) {
             if (err) {
-                /* TODO: Render the signup page, but with the values pre-filled so the person can press sign up again. */
-                res.status(500).send("There was a problem, please try again. ".concat(err));
+                res.status(500).send("There was a problem, please try again (refresh or go back). ".concat(err));
             } else {
                 res.redirect(303, '/success');
             }
         });
     } else {
-        /* TODO: Render the signup page, but with the values pre-filled so the person can press sign up again. */
-        res.render('signup', { activePage: 'signup', errors: to_error_dict(errors), title: 'Sign up for '.concat(config.slack_name), signup_description: config.signup_description, conditional: config.conditional, code_of_conduct: config.code_of_conduct, slack_name: config.slack_name, recaptcha_sitekey: config.recaptcha_sitekey });
+      var viewModel = {
+        activePage: "signup",
+        errors: to_error_dict(errors),
+        title: "Sign up for ".concat(config.slack_name),
+        signup_description: config.signup_description,
+        conditional: config.conditional,
+        code_of_conduct: config.code_of_conduct,
+        slack_name: config.slack_name,
+        recaptcha_sitekey: config.recaptcha_sitekey,
+        info: info
+      }
+
+      res.render('signup', viewModel);
     }
 });
 
